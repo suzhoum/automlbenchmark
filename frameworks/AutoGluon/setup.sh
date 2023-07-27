16e98a3681863e812b7af225eb408aed7cb76e28
innixma <innixma@gmail.com>
2023-07-27 00:51:53 +0000

    2023_07_25

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -27,7 +27,7 @@ elif [[ "$VERSION" =~ ^[0-9] ]]; then
     PIP install --no-cache-dir -U "${PKG}==${VERSION}"
     PIP install --no-cache-dir -U "${PKG}.tabular[skex]==${VERSION}"
 else
-    VERSION="fix_time_log_when_none"
+    VERSION="add_model_failures"
     REPO="https://github.com/Innixma/autogluon.git"
 
     TARGET_DIR="${HERE}/lib/${PKG}"
