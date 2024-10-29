0cc604c0f4a1178efc6e706db69bfa9e13f184c5
innixma <innixma@gmail.com>
2024-10-29 01:02:51 +0000

    refactor AutoGluon setup.sh to work with uv

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -3,6 +3,9 @@
 # exit when any command fails
 set -e
 
+# Uncomment for debugging installation
+# set -x
+
 HERE=$(dirname "$0")
 VERSION=${1:-"stable"}
 REPO=${2:-"https://github.com/autogluon/autogluon.git"}
@@ -14,6 +17,9 @@ fi
 # creating local venv
 . ${HERE}/../shared/setup.sh ${HERE} true
 
+# aka "{xyz}/automlbenchmark/frameworks/AutoGluon/venv/bin/python"
+PY_EXEC_NO_ARGS="$(cut -d' ' -f1 <<<"$py_exec")"
+
 # Below fixes seg fault on MacOS due to bug in libomp: https://github.com/autogluon/autogluon/issues/1442
 if [[ -x "$(command -v brew)" ]]; then
     wget https://raw.githubusercontent.com/Homebrew/homebrew-core/fb8323f2b170bd4ae97e1bac9bf3e2983af3fdb0/Formula/libomp.rb -P "${HERE}/lib"
@@ -21,22 +27,24 @@ if [[ -x "$(command -v brew)" ]]; then
 fi
 
 PIP install --upgrade pip
-PIP install --upgrade setuptools wheel
 
-PIP install s3fs
+PIP install uv
+UV="${PY_EXEC_NO_ARGS} -m uv"
+
+$UV pip install s3fs
 
 if [[ "$VERSION" == "latest_gpu" ]]; then
-    PIP install mmcv
-    PIP install pulp
-    PIP install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchtext==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
+    $UV pip install mmcv
+    $UV pip install pulp
+    $UV pip install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchtext==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
 fi
 
 if [[ "$VERSION" == "stable" ]]; then
-    PIP install --no-cache-dir -U "${PKG}"
-    PIP install --no-cache-dir -U "${PKG}.tabular[skex]"
+    $UV pip install --no-cache-dir -U "${PKG}"
+    $UV pip install --no-cache-dir -U "${PKG}.tabular[skex]"
 elif [[ "$VERSION" =~ ^[0-9] ]]; then
-    PIP install --no-cache-dir -U "${PKG}==${VERSION}"
-    PIP install --no-cache-dir -U "${PKG}.tabular[skex]==${VERSION}"
+    $UV pip install --no-cache-dir -U "${PKG}==${VERSION}"
+    $UV pip install --no-cache-dir -U "${PKG}.tabular[skex]==${VERSION}"
 else
 #    VERSION="2024_10_24_amlb"
 #    REPO="https://github.com/Innixma/autogluon.git"
@@ -45,13 +53,17 @@ else
     rm -Rf ${TARGET_DIR}
     git clone --depth 1 --single-branch --branch ${VERSION} --recurse-submodules ${REPO} ${TARGET_DIR}
     cd ${TARGET_DIR}
-    PY_EXEC_NO_ARGS="$(cut -d' ' -f1 <<<"$py_exec")"
     PY_EXEC_DIR=$(dirname "$PY_EXEC_NO_ARGS")
-    env PATH="$PY_EXEC_DIR:$PATH" bash -c ./full_install.sh
-    PIP install -e tabular/[skex]
+
+    # Install in non-editable mode to avoid interaction with other pre-existing AutoGluon installations
+    env PATH="$PY_EXEC_DIR:$PATH" bash -c "./full_install.sh --non-editable"
+    $UV pip install tabular/[skex]
 fi
 
 #PIP install s3fs
 #PIP install "boto3==1.26.90"
+
+# Note: `setuptools` being present in the venv will cause torch==1.4.x to raise an exception for an unknown reason in AMLB.
+echo "Finished setup, testing autogluon install..."
 
 PY -c "from autogluon.tabular.version import __version__; print(__version__)" >> "${HERE}/.setup/installed"
